7cde6ac2eda3aaf33361cc799b023f51579c464a
David Hoffman <dghoffman@smu.edu>
2021-10-20 21:12:30 -0500

    adding past functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,87 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+import { ROUTES } from './routes';
+import './Semantic-UI-CSS/semantic.min.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// React functional component
-function App () {
-  // state for storage of the information on the webpage of forms and list, uses hooks
-  const [number, setNumber] = useState("")
-  const [values, setValues] = useState([])
-
-  // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
-  // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
-  // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-  const url = ec2 ? ec2_url : 'localhost'
-
-  // handle input field state change
-  const handleChange = (e) => {
-    setNumber(e.target.value);
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Switch>
+            { ROUTES.map((route, index) => <Route key={index} { ...route } />) }
+          </Switch>
+        </Router>
+      </>
+    );
   }
-
-  const fetchBase = () => {
-    axios.get(`http://${url}:8000/`).then((res)=>{
-      alert(res.data);
-    })
-  }
-
-  // fetches vals of db via GET request
-  const fetchVals = () => {
-    axios.get(`http://${url}:8000/values`).then(
-      res => {
-        const values = res.data.data;
-        console.log(values);
-        setValues(values)
-    }).catch(err => {
-      console.log(err)
-    });
-  }
-
-  // handle input form submission to backend via POST request
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let prod = number * number;
-    axios.post(`http://${url}:8000/multplynumber`, {product: prod}).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-    setNumber("");
-  }
-
-  // handle intialization and setup of database table, can reinitialize to wipe db
-  const reset = () => {
-    axios.post(`http://${url}:8000/reset`).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-  }
-
-  // tell app to fetch values from db on first load (if initialized)
-  useEffect(() => {
-    fetchVals();
-  }, [])
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={fetchBase} style={{marginBottom: '1rem'}}> {`GET: http://${url}:8000/`} </button>
-        <button onClick={reset}> Reset DB </button>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={number} onChange={handleChange}/>
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          { values.map((value, i) => <li key={i}>{value.value}</li>) }
-        </ul>
-      </header>
-    </div>
-  );
 }
 
 export default App;
---
+++ frontend/src/Components/login.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './login.css'
+import { Redirect, Link } from 'react-router-dom';
+import logo from './logo.png';
+import { VacationGramAPIClient } from '../Api/VacationGramAPIClient';
+import { LoginButton, ErrorMessage } from './loginButton';
+
+class Login extends React.Component {
+
+    apiClient = new VacationGramAPIClient();
+
+    onLogin() {
+        this.apiClient.login(this.state.email, this.state.password).then(user => {
+            console.log(user.info[0].id)
+            if (user.info[0].id !== undefined) {
+                this.setState({ authenticated: true });
+                console.log(user.info[0]);
+                this.setState({ id: user.info[0].id });
+            }
+            else {
+                this.setState({ authenticated: false });
+            }
+        });
+
+    }
+    registerUser = e => {
+        this.setState({ register: true });
+    }
+
+    state = {
+        id: '',
+        username: "",
+        email: "",
+        password: "",
+        authenticated: null
+    };
+
+    render() {
+        return <>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137604632-1"></script>
+
+            {this.state.authenticated === false && <ErrorMessage />}
+            <div id="loginPageBG">
+            <div className="fadeInDown" id="loginWrapper">
+                <div id="loginFormContent">
+                    <div className="imgcontainer pt-4 mb-0">
+                        <h1>Welcome to TravelGram!</h1>
+                        <img src={logo} alt="Avatar" className="avatar"></img>
+                    </div>
+                    <div className="login-form">
+                        <div className="form-group">
+                            {/* <label htmlFor="search_name">Email</label> */}
+                            <input type="text"
+                                name="Email"
+                                className="form-control"
+                                placeholder="email"
+                                value={this.state.email}
+                                onChange={e => this.setState({ email: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="login-form">
+                        <div className="form-group">
+                            {/* <label htmlFor="search_name">Password</label> */}
+                            <input type="password"
+                                name="Password"
+                                className="form-control"
+                                placeholder="password"
+                                value={this.state.password}
+                                onChange={e => this.setState({ password: e.target.value })}
+                            />
+                        </div>
+                        <button className="btn btn-primary btn-lg btn-block" type="button"
+                            onClick={() => this.onLogin()}>Log In</button>
+                        {this.state.authenticated && <Redirect to={'/home/' + this.state.id} />}
+                        <br></br>
+                        <div className="pb-5">
+                        <Link to={'register'}>
+                            <button className="btn btn-primary btn-lg mb-7 btn-block" type="button">Register</button>
+                        </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            {/* <Link to={'/home/' + this.state.id}>Skip to Home</Link> */}
+        </>;
+    }
+
+}
+export default Login;
