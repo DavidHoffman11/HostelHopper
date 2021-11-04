b67a6f284e4f931deae98926be0a7bb75e167222
pkopser <pkopser@smu.edu>
2021-11-04 18:25:44 -0500

    Updated Front Page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,10 @@
 import Login from './Components/login';
 import RegisterPage from './Components/register';
 
+
 export const ROUTES = [
     { path: '/register', component: RegisterPage },
     { path: '/', component: Login },
 ]
+
+
