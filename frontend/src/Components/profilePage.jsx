b6b4a73693b049b361fe9263fa12ead178beab42
milesfried <mafriedman@smu.edu>
2021-11-18 16:56:52 -0600

    adding hostel obj

--- frontend/src/Components/profilePage.jsx
+++ frontend/src/Components/profilePage.jsx
@@ -4,8 +4,26 @@ import { Redirect, Link } from 'react-router-dom';
 import logo from '../logo.png';
 import { HostelHopperAPIClient } from '../Api/HostelHopperAPIClient';
 import { LoginButton, ErrorMessage } from './loginButton';
+import {Hostel} from '../Models/hostel';
+import { ReviewList } from './reviewList';
 
 export class ProfilePage extends React.Component {
+    state = {
+        Hostel: new Hostel(0, 
+            "Big Cabana Resort", 
+            "It is a nice place", 
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.summerwood.com%2Fproducts%2Fpool-cabanas%2Fsanara%2F231646&psig=AOvVaw37HlCLJd8Sj0S4PZtcKoaO&ust=1636763441502000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjH347JkfQCFQAAAAAdAAAAABAD",
+        1000, 
+        "it is cool", 
+        "we have good food", 
+        "big oceanside view", 
+        true, 
+        true, 
+        "California", 
+        true, 
+        true, 
+        []),
+    }
 
     apiClient = new HostelHopperAPIClient(); 
 
@@ -20,6 +38,7 @@ export class ProfilePage extends React.Component {
                 <img className="float-left" src="https://via.placeholder.com/150" alt="picture"></img>
                 <h1>Bio</h1>
                 <p>Insert bio here</p>
+                <ReviewList reviews={this.state.Hostel.reviews}/>
             </div>
         </>
     }
