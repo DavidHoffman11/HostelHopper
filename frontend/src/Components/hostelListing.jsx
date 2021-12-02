356f10902e04a6033ae24b4da01f706f76b52297
milesfried <mafriedman@smu.edu>
2021-12-01 21:52:39 -0600

    update hostelisting

--- frontend/src/Components/hostelListing.jsx
+++ frontend/src/Components/hostelListing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, onSearch } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HostelHopperAPIClient } from '../Api/HostelHopperAPIClient';
 import './homePage.css'
 import placeHolder from '../img_placeHolder.png';
@@ -7,16 +7,17 @@ import Hostel from '../Models/hostel';
 
 
 export const HostelListing = props => {
-  const [hostels, setHostels] = useState([]);
+  let hostels = [];
   const apiClient = new HostelHopperAPIClient(); 
   
   useEffect(() => {
     onSearch();
-  }, []);
+  });
   
   let onSearch = params => {
-    apiClient.getAllHosts(params).then(x => setHostels(x));
-    console.log(hostels);
+    var hostels;
+    apiClient.getAllHosts().then(x => hostels = x);
+    console.log();
   }
   
   
@@ -30,16 +31,16 @@ export const HostelListing = props => {
     <ul id = "hostel-list">
   {
         hostels.map(hostel => <div key = {hostel.id} className="card" id="listing">
-  
-        <h3 class="hostelName" >{hostel.hostelName}</h3>
-  
+   
+        <h3 className="hostelName" >{hostel.hostelName}</h3>
+   
         <img src={hostel.profilePicUrl} alt="Avatar" className="avatar" id="hostelImage"></img>
       
-        <div class="card-body">
-          <h5 class="card-title">Price / Night:{hostel.pricing}</h5>
-          <p class="card-text">{hostel.info}</p>
+        <div className="card-body">
+          <h5 className="card-title">Price / Night:{hostel.pricing}</h5>
+          <p className="card-text">{hostel.info}</p>
           <Link to={ `homepage/${hostel.id}` }>
-          <a href="#" class="btn btn-primary">View this hostel</a>
+          <a href="#" className="btn btn-primary">View this hostel</a>
           </Link>
         </div>
       </div>)
