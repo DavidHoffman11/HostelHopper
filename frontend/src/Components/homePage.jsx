578ab90771c384244a507a8b6eee4e586acd8001
pkopser <pkopser@smu.edu>
2021-12-02 16:06:52 -0600

    HomePage UI

--- frontend/src/Components/homePage.jsx
+++ frontend/src/Components/homePage.jsx
@@ -20,7 +20,7 @@ export class HomePage extends React.Component {
         <div className="background">
           <div id="homePageHeader">
           <Link to={ `/profile/${this.props.match.params.id}` }>
-                                <button className="btn btn-primary btn-lg mb-7 " type="button">Profile</button>
+                                <button className="btn btn-primary btn-lg mb-7 " type="button" id="backToProfile">Go to my Profile</button>
                             </Link>
             Avaliable Hostels 
           </div >
@@ -29,20 +29,21 @@ export class HomePage extends React.Component {
 
           <div className="input-group rounded" id="searchBar">
 
-            <select id="filters" name="filters" value={this.state.filter} onChange={e => this.setState({filter: e.target.value})}>
-            <option value=""></option>
-              <option value="Pet Friendly">Pet Friendly</option>
-              <option value="Price">Price(Lowest to Highest)</option>
-              <option value="Has Lockers">Has Lockers</option>
-              <option value="COVID-Safe">COVID-Safe</option>
-              <option value="Gender-Separated">Gender-Separated</option>
+            <select id="filters" class="filterBar" name="filters" value={this.state.filter} onChange={e => this.setState({filter: e.target.value})}>
+            <option value="" >Filter by...</option>
+              <option value="Pet Friendly">Pet Friendly Hostels</option>
+              <option value="Price"> Price(Lowest to Highest)</option>
+              <option value="Has Lockers">Hostels with lockers</option>
+              <option value="COVID-Safe">COVID-Safe Hostels</option>
+              <option value="Gender-Separated">Gender-Separated Hostels</option>
               </select>
-            <span className="input-group-text border-0" id="search-addon">
-              </span>
+           
           </div>
-        </div> 
-        <HostelListing userID ={this.props.match.params.id} filter ={this.state.filter}/>
 
+        
+       
+        <HostelListing userID ={this.props.match.params.id} filter ={this.state.filter}/>
+</div>
         
       </>
     }
